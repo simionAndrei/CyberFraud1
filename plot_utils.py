58d87cd83b8b2d45b7ae21c0abff5fe0610369b8
Simion-Constantinescu Andrei <andrei.simion.c@gmail.com>
2019-05-09 16:52:13 +0200

    Sync with master

--- plot_utils.py
+++ plot_utils.py
@@ -3,23 +3,18 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-def create_heat_map(plt_title, data_df, filename, logger):
+def create_heat_map(plt_title, corr_df, feats_names, filename, logger):
 
-  #fig = plt.figure(figsize=(7, 7))
+  fig = plt.figure(figsize=(8, 8))
 
   colormap = sns.diverging_palette(220, 10, as_cmap=True)
-  sns.heatmap(data_df.corr(), cmap = colormap, annot = True, fmt = ".2f")
+  sns.heatmap(corr_df, cmap = colormap, annot = True, fmt = ".2f")
 
-  x = np.array(range(data_df.shape[0]))
-  plt.xticks(x, data_df.columns)
-  plt.yticks(x, data_df.columns)
+  x = np.array(range(len(feats_names)))
+  plt.xticks(x, feats_names)
+  plt.yticks(x, feats_names)
 
-  '''
   plt.savefig(logger.get_output_file(filename), dpi = 120, 
     bbox_inches='tight')
-  plt.close()
-  '''
-
-  plt.show()
 
 
